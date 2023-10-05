ac8cfc385c42372b000c6a09b55117d3c59796e0
Ijustaggedzw <zwhite2023@elam.co.uk>
2023-10-05 15:38:40 +0100

    JUMPFORCE AND GROUDCHECK
    ADDED JUMPFORCE AND A SEMI GROUNDCHECKER

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,18 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject groundChecker;
     public LayerMask whatIsGround;
-    
+    public float jumpForce;
     float maxSpeed = 5.0f;
-    bool isOnGround = false;
-   
-    
+    public bool isOnGround = false;
+
+
     Rigidbody2D playerObject;
-  
+
     // Start is called before the first frame update
     void Start()
     {
-      
-        
+
+
         playerObject = GetComponent<Rigidbody2D>();
     }
 
@@ -25,9 +25,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float movementValueX = Input.GetAxis("Horizontal");
-       
-       playerObject.velocity = new Vector2 (movementValueX*maxSpeed, playerObject.velocity.y);
+
+        playerObject.velocity = new Vector2(movementValueX * maxSpeed, playerObject.velocity.y);
 
         isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+
+        if ((isOnGround == true) && (Input.GetAxis("Jump") > 0.0f))
+
+        {
+            playerObject.AddForce(Vector2.up * jumpForce);
+        }
+
     }
 }
